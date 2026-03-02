d80e7764df26279fdd541a49b06066f3b1554f9d
VinzentRisch <risch.vinzent@gmail.com>
2026-03-02 16:30:37 +0100

    change tests

--- q2_mfa/tests/test_transform.py
+++ q2_mfa/tests/test_transform.py
@@ -16,28 +16,29 @@ from q2_mfa.transform import transform_clr
 class TestTransformCLR(TestPluginBase):
     package = "q2_mfa.tests"
 
-    def test_transform_clr_data_adaptive_uses_min_nonzero(self):
-        df = pd.DataFrame(
+    def setUp(self):
+        super().setUp()
+
+        self.df = pd.DataFrame(
             [[0, 1], [2, 3]],
         )
 
-        exp = pd.DataFrame(
-            [[-0.346574, 0.346574], [-0.143841, 0.143841]],
-        )
+    def test_transform_clr_data_adaptive_uses_min_nonzero(self):
+        # Apply pseudocount of 1 (minimum nonzero value)
+        df_adjusted = self.df + 1
+        # Calculate CLR: log(x) - mean(log(x))
+        log_df = np.log(df_adjusted)
+        exp = log_df.sub(log_df.mean(axis=1), axis=0)
 
-        obs = transform_clr(df, pseudocount=None)
+        obs = transform_clr(self.df, pseudocount=None)
         pd.testing.assert_frame_equal(obs, exp)
-        assert np.allclose(obs.sum(axis=1).to_numpy(), 0.0)
 
     def test_transform_clr_fixed_pseudocount(self):
-        df = pd.DataFrame(
-            [[0, 1], [2, 3]],
-        )
+        # Apply pseudocount of 0.5
+        df_adjusted = self.df + 0.5
+        # Calculate CLR: log(x) - mean(log(x))
+        log_df = np.log(df_adjusted)
+        exp = log_df.sub(log_df.mean(axis=1), axis=0)
 
-        exp = pd.DataFrame(
-            [[-0.549306, 0.549306], [-0.168236, 0.168236]],
-        )
-
-        obs = transform_clr(df, pseudocount=0.5)
+        obs = transform_clr(self.df, pseudocount=0.5)
         pd.testing.assert_frame_equal(obs, exp)
-        assert np.allclose(obs.sum(axis=1).to_numpy(), 0.0)
